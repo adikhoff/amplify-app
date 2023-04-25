940ca0297204b0f304656aae146d1b923f84b7db
Arjan Dikhoff <dikhoff@gmail.com>
2023-04-25 16:20:32 +0200

    Everything working now

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "RestaurantAPI": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyapp18ea8822": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { Amplify, Auth } from 'aws-amplify';
+import awsmobile from './aws-exports';
 
+Amplify.configure(awsmobile);
+Auth.configure(awsmobile);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
