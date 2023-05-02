135b38dededbcf938d390f3d1d723fdcdd53d744
Arjan Dikhoff <dikhoff@gmail.com>
2023-05-02 14:56:19 +0200

    Add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Trigger6abc9819": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "photos": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
