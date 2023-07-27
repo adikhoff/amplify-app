936c3d8c6d156b12cc2dbe660eff3031c527772f
Arjan Dikhoff <dikhoff@gmail.com>
2023-07-27 19:31:46 +0200

    Remove autorefresh

--- src/app/logged-in-container/logged-in-container.component.ts
+++ src/app/logged-in-container/logged-in-container.component.ts
@@ -9,8 +9,6 @@ import {PhotoService} from "../service/photo-service";
   styleUrls: ['./logged-in-container.component.css']
 })
 export class LoggedInContainerComponent implements OnInit {
-  private SLEEP_THRESHOLD = 5000;
-
   public user: any;
 
   constructor(
@@ -23,27 +21,10 @@ export class LoggedInContainerComponent implements OnInit {
   };
 
   async ngOnInit() {
-    console.log("Init wakeup service...");
-    this.initWakeupService();
     console.log("Waiting for user...");
     let user = await Auth.currentAuthenticatedUser();
     console.log("Found user, setup credentials", user);
     this.userService.setupCredentials();
     console.log("Set up credentials");
-  }
-
-  private initWakeupService() {
-    let now = new Date().getTime();
-    setInterval (() => {
-      if ((new Date().getTime() - now) > this.SLEEP_THRESHOLD) {
-        console.log ('wake-up from sleep detected');
-        this.refreshCaches();
-      }
-      now = new Date().getTime();
-    }, 1000);
-  }
-
-  private refreshCaches() {
-    this.photoService.refresh();
   }
 }
