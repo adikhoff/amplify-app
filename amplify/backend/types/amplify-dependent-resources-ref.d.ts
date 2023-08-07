c0bb52ee638a6204be3a9c49a7aba60ee5e80bb2
Arjan Dikhoff <dikhoff@gmail.com>
2023-08-07 22:50:58 +0200

    Try to resurrect dev

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,37 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dekkerreunie": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "dekkerreuniee387ba55": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger6abc9819": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "photos": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
